49dbfbb9b2c2569ede19baf4b9ced53225c1cd73
Júlio Cesar <juliocesar27072004@outlook.com>
2022-10-11 18:42:42 -0300

    SecondWindow, Button

--- main.c
+++ main.c
@@ -1,43 +1,82 @@
 #include "raylib.h"
 
+#define windowCenter 195
+#define screenWidth 450
+#define screenHeight 500
+
+
+
+void firstWindow(){
+
+    // IMAGE
+    Image img = LoadImage("resources/ghost_chase.gif");
+    Texture2D imgTexture = LoadTextureFromImage(img);
+
+    // TEXTS
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("PAC-MAN", windowCenter - 20, 10, 20, WHITE);
+    DrawTexture(imgTexture, GetScreenWidth() / 2 - imgTexture.width / 2, 140, WHITE);
+    DrawRectangleLines(windowCenter - 70, 285, 200, 50, WHITE);
+    DrawText("PLAY", windowCenter, 300, 20, DARKGRAY);
+    DrawRectangleLines(windowCenter - 70, 385, 200, 50, WHITE);
+    DrawText("SCORE", windowCenter, 400, 20, DARKGRAY);
+    EndDrawing();
+}
+
+void secondWindow() {
+
+    BeginDrawing();
+    DrawText("CLICK", windowCenter, 100, 20, DARKGRAY);
+    EndDrawing();
+}
+
+void actions() {
+    // BTN
+    Rectangle btnBounds = {windowCenter - 70, 285, 200, 50};
+    int btnState = 0;       // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+    bool btnAction = false; // Button action should be activated
+
+    Vector2 mousePoint = {0.0f, 0.0f}; // MOUSE POINTER
+
+    // BTN CONDITIONS
+    mousePoint = GetMousePosition();
+    btnAction = false;
+
+    // Check button state
+    if (CheckCollisionPointRec(mousePoint, btnBounds))
+    {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            btnAction = true;
+    }
+    else
+        btnState = 0;
+
+    if (btnAction) // DO THE NEXT ACTION
+    {
+        secondWindow();
+    }
+
+}
+
+int state = 0;
 
 int main(void)
 {
+    InitWindow(screenWidth, screenHeight, "Game");
 
-    const int screenWidth = 450;
-    const int screenHeight = 500;
-    const int windowCenter = ((screenWidth / 2) - 30);
+    //Image icon = LoadImage("resources/window_icon.gif");
+    //SetWindowIcon(icon); // Window Icon
 
-
-    InitWindow(screenWidth, screenHeight, "Pac-Man");
-
-    Image img = LoadImage("resources/ghost_chase.gif");
-
-    Texture2D texScarfyAnim = LoadTextureFromImage(img);
-
-    SetTargetFPS(60);
+    SetTargetFPS(60); // FPS
 
     while (!WindowShouldClose()) 
     {
-     
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-
-        DrawText("PAC-MAN", windowCenter - 20, 10, 20, WHITE);
-
-        DrawTexture(texScarfyAnim, GetScreenWidth() / 2 - texScarfyAnim.width / 2, 140, WHITE);
-
-        DrawRectangleLines(windowCenter - 70, 285, 200, 50, WHITE);
-        DrawText("PLAY", windowCenter, 300, 20, DARKGRAY);
-        DrawRectangleLines(windowCenter - 70, 385, 200, 50, WHITE);
-        DrawText("EXIT", windowCenter, 400, 20, DARKGRAY);
-
-        EndDrawing();
+        firstWindow();
+        actions();
     }
 
-    CloseWindow(); 
+    CloseWindow();
 
     return 0;
 }
