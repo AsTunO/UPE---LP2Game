672db0c5cd5d332b3e22fd957efd04bff70e2584
Júlio Cesar <juliocesar27072004@outlook.com>
2022-10-11 17:26:44 -0300

    FirstWindow

--- main.c
+++ main.c
@@ -6,11 +6,14 @@ int main(void)
 
     const int screenWidth = 450;
     const int screenHeight = 500;
-    const int windowCenter = ((screenWidth / 2) - 50);
-
+    const int windowCenter = ((screenWidth / 2) - 30);
 
 
     InitWindow(screenWidth, screenHeight, "Pac-Man");
+
+    Image img = LoadImage("resources/ghost_chase.gif");
+
+    Texture2D texScarfyAnim = LoadTextureFromImage(img);
 
     SetTargetFPS(60);
 
@@ -22,12 +25,14 @@ int main(void)
         ClearBackground(BLACK);
 
 
-        DrawText("PAC-MAN", windowCenter, 10, 20, WHITE);
+        DrawText("PAC-MAN", windowCenter - 20, 10, 20, WHITE);
 
-        DrawRectangleLines(windowCenter-50, 185, 200, 50, WHITE);
-        DrawText("PLAY", windowCenter+20, 200, 20, DARKGRAY);
-        DrawRectangleLines(windowCenter - 50, 385, 200, 50, WHITE);
-        DrawText("EXIT", windowCenter+20, 400, 20, DARKGRAY);
+        DrawTexture(texScarfyAnim, GetScreenWidth() / 2 - texScarfyAnim.width / 2, 140, WHITE);
+
+        DrawRectangleLines(windowCenter - 70, 285, 200, 50, WHITE);
+        DrawText("PLAY", windowCenter, 300, 20, DARKGRAY);
+        DrawRectangleLines(windowCenter - 70, 385, 200, 50, WHITE);
+        DrawText("EXIT", windowCenter, 400, 20, DARKGRAY);
 
         EndDrawing();
     }
